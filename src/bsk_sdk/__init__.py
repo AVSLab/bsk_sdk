c75f2a3a3624e4310b308ece2aa94024688dfaef
Reece Humphreys <reecewh@icloud.com>
2026-03-27 21:00:54 -0600

    Add doc comment

--- src/bsk_sdk/__init__.py
+++ src/bsk_sdk/__init__.py
@@ -1,3 +1,42 @@
+#
+#  ISC License
+#
+#  Copyright (c) 2026, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""
+Basilisk SDK (``bsk-sdk``) -- build Basilisk-compatible SWIG plugins out-of-tree.
+
+This package ships curated Basilisk headers, minimal runtime sources,
+SWIG interface files, and CMake helpers so that external projects can
+compile and link Basilisk plugins without vendoring the full simulation
+codebase.
+
+Quick start::
+
+    pip install bsk-sdk
+
+Then in your plugin's ``CMakeLists.txt``::
+
+    find_package(bsk-sdk CONFIG REQUIRED)
+    bsk_add_swig_module(TARGET myPlugin INTERFACE swig/myPlugin.i ...)
+
+Convenience functions below expose installed paths for headers, SWIG
+support files, CMake config, and tools.
+"""
+
 from importlib import resources
 from pathlib import Path
 
