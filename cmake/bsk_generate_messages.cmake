0611a3f5fa817a958ff4898ae021862017488eb7
Reece Humphreys <reecewh@icloud.com>
2026-03-27 20:32:00 -0600

    Simplify cmake

--- cmake/bsk_generate_messages.cmake
+++ cmake/bsk_generate_messages.cmake
@@ -1,16 +1,33 @@
+#
+#  ISC License
+#
+#  Copyright (c) 2026, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
 include_guard(GLOBAL)
-include(CMakeParseArguments)
 
 if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/bsk_add_swig_module.cmake")
   include("${CMAKE_CURRENT_LIST_DIR}/bsk_add_swig_module.cmake")
 endif()
 
+# Locate the msgAutoSource directory that ships generateSWIGModules.py.
 function(_bsk_resolve_msg_autosource_dir out_var)
-  if(DEFINED BSK_SDK_MSG_AUTOSOURCE_DIR)
-    if(EXISTS "${BSK_SDK_MSG_AUTOSOURCE_DIR}/generateSWIGModules.py")
-      set(${out_var} "${BSK_SDK_MSG_AUTOSOURCE_DIR}" PARENT_SCOPE)
-      return()
-    endif()
+  if(DEFINED BSK_SDK_MSG_AUTOSOURCE_DIR
+      AND EXISTS "${BSK_SDK_MSG_AUTOSOURCE_DIR}/generateSWIGModules.py")
+    set(${out_var} "${BSK_SDK_MSG_AUTOSOURCE_DIR}" PARENT_SCOPE)
+    return()
   endif()
 
   if(DEFINED bsk-sdk_DIR AND EXISTS "${bsk-sdk_DIR}")
@@ -21,11 +38,9 @@ function(_bsk_resolve_msg_autosource_dir out_var)
 
   get_filename_component(_pkg_root "${_cmake_dir}/../../.." REALPATH)
 
-  set(_cand_installed "${_pkg_root}/tools/msgAutoSource")
-  set(_cand_source "${_cmake_dir}/../tools/msgAutoSource")
   set(_candidates
-    "${_cand_installed}"
-    "${_cand_source}"
+    "${_pkg_root}/tools/msgAutoSource"
+    "${_cmake_dir}/../tools/msgAutoSource"
   )
 
   foreach(_cand IN LISTS _candidates)
@@ -40,19 +55,10 @@ function(_bsk_resolve_msg_autosource_dir out_var)
     "bsk-sdk message generator not found.\n\n"
     "Looked for generateSWIGModules.py under:\n"
     "  ${_cand_list}\n\n"
-    "Resolver context:\n"
-    "  bsk-sdk_DIR = ${bsk-sdk_DIR}\n"
-    "  CMAKE_CURRENT_FUNCTION_LIST_DIR = ${CMAKE_CURRENT_FUNCTION_LIST_DIR}\n"
-    "  (note: caller CMAKE_CURRENT_LIST_DIR is irrelevant here)\n\n"
-    "If using an installed wheel, ensure it installs:\n"
-    "  bsk_sdk/tools/msgAutoSource/generateSWIGModules.py\n"
-    "If building from source, ensure this exists:\n"
-    "  sdk/tools/msgAutoSource/generateSWIGModules.py\n"
     "Or set BSK_SDK_MSG_AUTOSOURCE_DIR explicitly."
   )
 endfunction()
 
-# Main
 function(bsk_generate_messages)
   set(options GENERATE_C_INTERFACE)
   set(oneValueArgs OUTPUT_DIR OUT_VAR)
@@ -67,29 +73,16 @@ function(bsk_generate_messages)
     set(BSK_OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}")
   endif()
 
-  find_package(Python3 QUIET COMPONENTS Interpreter)
-  _bsk_setup_pip_swig("${Python3_EXECUTABLE}")
-
-  find_package(SWIG REQUIRED COMPONENTS python)
-  include(${SWIG_USE_FILE})
-  find_package(Python3 REQUIRED COMPONENTS Interpreter Development.Module NumPy)
-  find_package(Eigen3 CONFIG REQUIRED)
+  _bsk_find_build_deps()
 
   if(NOT BSK_TARGET_LINK_LIBS)
-    if(TARGET bsk::arch_min)
-      set(BSK_TARGET_LINK_LIBS "bsk::arch_min")
-    else()
-      _bsk_resolve_basilisk_libs(_bsk_libs)
-      set(BSK_TARGET_LINK_LIBS "${_bsk_libs}")
-    endif()
+    _bsk_resolve_link_libs(BSK_TARGET_LINK_LIBS)
   endif()
 
-
   _bsk_collect_swig_flags(_swig_flags)
-
   _bsk_resolve_msg_autosource_dir(_msg_autosrc)
-  set(_gen_swig "${_msg_autosrc}/generateSWIGModules.py")
 
+  set(_gen_swig "${_msg_autosrc}/generateSWIGModules.py")
   set(_auto_root "${CMAKE_CURRENT_BINARY_DIR}/autoSource")
   set(_xml_dir "${_auto_root}/xmlWrap")
   file(MAKE_DIRECTORY "${_xml_dir}")
@@ -105,6 +98,7 @@ function(bsk_generate_messages)
     get_filename_component(_payload_name "${_hdr_abs}" NAME_WE)
     get_filename_component(_hdr_dir "${_hdr_abs}" DIRECTORY)
 
+    # Generate SWIG XML from the header
     set(_xml_out "${_xml_dir}/${_payload_name}.xml")
     add_custom_command(
       OUTPUT "${_xml_out}"
@@ -114,6 +108,7 @@ function(bsk_generate_messages)
       VERBATIM
     )
 
+    # Generate the .i interface file from the XML
     set(_i_out "${_auto_root}/${_payload_name}.i")
     add_custom_command(
       OUTPUT "${_i_out}"
@@ -128,9 +123,10 @@ function(bsk_generate_messages)
       VERBATIM
     )
 
+    # Build the SWIG module
     set_property(SOURCE "${_i_out}" PROPERTY CPLUSPLUS ON)
     set_property(SOURCE "${_i_out}" PROPERTY USE_TARGET_INCLUDE_DIRECTORIES TRUE)
-    set_property(SOURCE "${_i_out}" PROPERTY SWIG_FLAGS ${_swig_flags})  # LIST, not string
+    set_property(SOURCE "${_i_out}" PROPERTY SWIG_FLAGS ${_swig_flags})
 
     swig_add_library(
       ${_payload_name}
@@ -141,42 +137,15 @@ function(bsk_generate_messages)
       OUTFILE_DIR "${_auto_root}"
     )
 
-    target_include_directories(${_payload_name} PRIVATE
-      ${BSK_INCLUDE_DIRS}
-      ${_hdr_dir}
-      "${BSK_SDK_INCLUDE_DIR}"
-      "${BSK_SDK_INCLUDE_DIR}/Basilisk"
-      "${BSK_SDK_INCLUDE_DIR}/Basilisk/architecture"
-      "${BSK_SDK_INCLUDE_DIR}/Basilisk/architecture/_GeneralModuleFiles"
-      $<$<BOOL:${BSK_SDK_COMPAT_INCLUDE_DIR}>:${BSK_SDK_COMPAT_INCLUDE_DIR}>
-      ${Python3_INCLUDE_DIRS}
-      ${Python3_NumPy_INCLUDE_DIRS}
+    _bsk_configure_swig_target(
+      ${_payload_name} "${BSK_OUTPUT_DIR}" "${BSK_TARGET_LINK_LIBS}"
+      "${BSK_INCLUDE_DIRS};${_hdr_dir}"
     )
-
-    target_link_libraries(${_payload_name} PRIVATE
-      Python3::Module
-      Eigen3::Eigen
-      ${BSK_TARGET_LINK_LIBS}
-    )
-
-    set_target_properties(${_payload_name} PROPERTIES
-      POSITION_INDEPENDENT_CODE ON
-      LIBRARY_OUTPUT_DIRECTORY "${BSK_OUTPUT_DIR}"
-      RUNTIME_OUTPUT_DIRECTORY "${BSK_OUTPUT_DIR}"
-    )
-    # On MSVC multi-config generators the per-config variants take precedence and
-    # default to appending Release/Debug/... subdirectories. Override them so the
-    # output always lands in BSK_OUTPUT_DIR regardless of generator.
-    foreach(_cfg RELEASE DEBUG RELWITHDEBINFO MINSIZEREL)
-      set_target_properties(${_payload_name} PROPERTIES
-        LIBRARY_OUTPUT_DIRECTORY_${_cfg} "${BSK_OUTPUT_DIR}"
-        RUNTIME_OUTPUT_DIRECTORY_${_cfg} "${BSK_OUTPUT_DIR}"
-      )
-    endforeach()
 
     list(APPEND _generated_targets ${_payload_name})
   endforeach()
 
+  # Generate __init__.py that re-exports all message payloads
   file(MAKE_DIRECTORY "${BSK_OUTPUT_DIR}")
   set(_init_file "${BSK_OUTPUT_DIR}/__init__.py")
   file(WRITE "${_init_file}" "")
